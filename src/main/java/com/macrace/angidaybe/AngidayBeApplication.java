6fa59177cb87b8f38206170a1cd7d21b6d9d5381
Vu The Huy <vuthehuy.t61@gmail.com>
2024-03-03 12:50:50 +0700

    feat: initial project

---
+++ src/main/java/com/macrace/angidaybe/AngidayBeApplication.java
@@ -0,0 +1,13 @@
+package com.macrace.angidaybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngidayBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngidayBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macrace/angidaybe/AngidayBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macrace.angidaybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngidayBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
